cecc3675e0f981d0e85282a2fc3622ff414b36a7
Manroop <manroop.singh@procdna.com>
2025-09-17 18:20:29 +0530

    push

--- packages/ui/src/Navbar.tsx
+++ packages/ui/src/Navbar.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 export function Navbar() {
   return (
     <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
-      <Link href="https://mfe-home.vercel.app/">Home</Link> |{" "}
-      <Link href="https://mfe-login.vercel.app/">Login</Link> |{" "}
+      <Link href="https://mfe-home-tawny.vercel.app/">Home</Link> |{" "}
+      <Link href="https://mfe-login-zeta.vercel.app/">Login</Link> |{" "}
       <Link href="https://mfe-admin.vercel.app/">Admin</Link>
     </nav>
   );
